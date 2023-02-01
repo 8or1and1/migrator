026b98dde83b117a2543537598ea40d67f16454a
8or1and1 <8or1and1@gmail.com>
2023-02-01 18:39:13 +0300

    v0.5

--- testScript.py
+++ testScript.py
@@ -3,9 +3,9 @@ from elmaConnector import elmaConnector
 
 
 
-elma = elmaConnector(config.elma_to_config, "_system_catalogs", "test_smeny_tipa_polya")
-
-
-x = elma.get_data()
-for y in x:
-    print(y['rol'])
+# elma = elmaConnector(config.elma_to_config, "_system_catalogs", "test_smeny_tipa_polya")
+#
+#
+# x = elma.get_data()
+# for y in x:
+#     print(y)
